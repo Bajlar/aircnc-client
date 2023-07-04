d645a21f0627c7da1380f75a47f7f7575bfd1b21
Bajlar01~ <bajlarrahman24@gmail.com>
2023-07-04 20:23:22 +0600

    first part is done

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from 'query-string';
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params, setParams] = useSearchParams();
+  // const category = params.get("category");
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+    const updateQuery = {
+      ...currentQuery,
+      category: label,
+    } 
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updateQuery,
+      },
+      {
+        skipNull: true,
+      }
+    );
+    navigate(url);
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center gap-2 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500">
+    <div onClick={handleClick} className="flex flex-col items-center justify-center gap-2 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500 cursor-pointer">
       <Icon size={26} />
-      <div className='text-sm font-medium'>{label}</div>
+      <div className="text-sm font-medium">{label}</div>
     </div>
   );
 };
--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -2,23 +2,34 @@ import React, { useEffect, useState } from 'react';
 import Container from '../Shared/Container/Container';
 import Card from './Card';
 import Loader from '../Shared/Loader/Loader';
+import { useSearchParams } from 'react-router-dom';
+import Heading from '../Heading/Heading';
 
 const Rooms = () => {
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+  // console.log(category);
   const [rooms, setRooms] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    fetch('./rooms.json')
-      .then(res => res.json())
-      .then(data => {
-        setRooms(data);
+    fetch("./rooms.json")
+      .then((res) => res.json())
+      .then((data) => {
+        if (category) {
+          const filtered = data.filter((room) => room.category === category); 
+          setRooms(filtered);
+        }
+        else {
+          setRooms(data);
+        }
         setLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }, [])
+      });
+  }, [category]);
   
   if (loading) {
     return <Loader />;
@@ -26,11 +37,21 @@ const Rooms = () => {
 
   return (
     <Container>
-      <div className="pt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {rooms.map((room, index) => (
-          <Card key={index} room={room} />
-        ))}
-      </div>
+      {rooms && rooms.length > 0 ? (
+        <div className="pt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {rooms.map((room, index) => (
+            <Card key={index} room={room} />
+          ))}
+        </div>
+      ) : (
+        <div className="pt-12">
+          <Heading
+            title="No Rooms Available in This Category!"
+            subTitle="Please Select Other Category"
+            center={true}
+          />
+        </div>
+      )}
     </Container>
   );
 };
